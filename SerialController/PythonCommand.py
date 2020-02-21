934b0123b8cfb44bf34284b4f02e058fe522d52a
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2020-02-21 01:12:08 -0400

    Add dumb eggs function

--- SerialController/PythonCommand.py
+++ SerialController/PythonCommand.py
@@ -1141,6 +1141,51 @@ class AutoMaxRaid(FindNStar):
 		self.battle(dynamax = self.dynamax)
 
 
+# CONDITIONS:
+# start in the bridge field 
+# on your bike
+# with a full party (the pokemon in the second slot will be replaced)
+# with the default menu option being the map
+# choose where new pokemon go *manually* in game settings 
+class DumbEggHatcher(PythonCommand):
+	def __init__(self, name):
+		super(DumbEggHatcher, self).__init__(name)
+
+	def do(self):
+		while True:
+			self.press(Button.X, wait=1.5) # open menu
+			self.press(Button.A, wait=2) # select map
+			self.press(Button.A, duration=1) # you want to teleport here?
+			self.press(Button.A, wait=4) # sure!
+
+			# walk to daycare and get an egg
+			self.press(Direction.DOWN, duration=0.05, wait=0.5)
+			self.press(Direction.DOWN, duration=0.8)
+			self.press(Direction.LEFT, duration=0.2)
+			self.press(Button.A, duration=1) # talk to her "I have an egg for you, do you want it?"
+			self.press(Button.A, wait=4) # yes I do
+			self.press(Button.A, wait=3) # you got it
+			self.press(Button.A, duration=1) # Put egg on your team
+			self.press(Button.A, wait=2) # please select the slot!
+			self.press(Direction.DOWN) # select correct pokemon slot
+			self.press(Button.A)
+			self.pressRep(Button.B, 6, duration=0.5, interval=0.5, wait=1)
+
+			# begin the spin
+			self.press(Direction.RIGHT, duration=1)
+			self.hold([Direction.RIGHT, Direction.R_LEFT])
+			for i in range(1, 53):
+				print('wait for ' + str(i))
+				self.wait(1)
+
+			print('egg hatching')
+			self.holdEnd([Direction.RIGHT, Direction.R_LEFT])
+
+			# egg hatched?
+			self.press(Button.A, wait=17) # Oh
+			self.press(Button.A, wait=4) # "Pokemon" hatched from the egg
+
+
 # Add commands you want to use
 # 使用したいコマンドをここに追加してください
 commands = {
@@ -1164,6 +1209,7 @@ commands = {
 	'Reset Game': RestartGame,
 	'Find N-Star Battle': FindNStar,
 	'Auto Max Raid Battle': AutoMaxRaid,
+	'DUMB EGGS': DumbEggHatcher,
 }
 
 # Add commands as utility you want to use
