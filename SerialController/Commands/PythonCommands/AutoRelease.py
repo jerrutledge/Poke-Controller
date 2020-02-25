e30eb4c5a438b650bc933bf752d8c33b2c6897c5
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2020-02-25 08:57:43 -0400

    Skip pokemon with perfect ivs in auto release

--- SerialController/Commands/PythonCommands/AutoRelease.py
+++ SerialController/Commands/PythonCommands/AutoRelease.py
@@ -14,37 +14,103 @@ class AutoRelease(ImageProcPythonCommand):
 		self.col = 6
 		self.cam = cam
 
+		# release a number of boxes in a row
+		self.boxes = 9
+		self.ocr_fails = 0
+		self.shinies = 0
+
 	def do(self):
+		if self.boxes > 1:
+			print("Releasing " + str(self.boxes) + " boxes...")
 		self.wait(0.5)
 
-		for i in range(0, self.row):
-			for j in range(0, self.col):
-				if not self.cam.isOpened():
-					self.Release()
-				else:
-					# if shiny, then skip
-					if not self.isContainTemplate('shiny_mark.png', threshold=0.9):
-						if self.isContainTemplate('status.png', threshold=0.7): # Maybe this threshold works for only Japanese version.
-							# Release a pokemon
-							self.Release()
+		for box in range(self.boxes):
+			if self.boxes > 1:
+				print("Releasing box #" + str(box + 1))
+			for i in range(self.row):
+				for j in range(self.col):
+					if not self.cam.isOpened():
+						self.Release()
+					else:
+						# delay for video feed to update
+						self.wait(1.4)
+						if self.isContainTemplate('status.png', threshold=0.7):
+							shiny = self.isContainTemplate('shiny_mark.png', threshold=0.9)
+							if shiny:
+								print("SHINY !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+								self.shinies += 1
+							# use OCR to check for perfect ivs
+							stats = []
+							judgements = ["No good", "Decent", "Pretty good", \
+								"Very good", "Fantastic", "Best"]
+							ocr_fail = True
+							# there is a small chance the OCR will fail
+							# therefore we check a max three times, breaking
+							# if the format is correct
+							for k in range(3):
+								text = self.getText(135, 350, 990, 1)
+								stats = text.split('\n')
+								while '' in stats:
+									stats.remove('')
+								print("stats="+str(stats))
+								if len(stats) == 6 and set(stats).issubset(judgements):
+									# ocr has read correct format!
+									ocr_fail = False
+									break
+								if k == 2:
+									print("OCR fail")
+									perfect_iv = True
+							perfect_iv = stats == ['Best', 'Best', 'Best', \
+									'Best', 'Best', 'Best']
+							# if it has perfect ivs, mark it and skip
+							if perfect_iv:
+								print("Perfect IVs!")
+								self.MarkPerfect()
+							# if shiny, then skip
+							elif ocr_fail:
+								print("OCR Fail, moving on...")
+								self.ocr_fails += 1
+							elif not shiny:
+								# Release a pokemon
+								self.Release()
 
+					if not j == self.col - 1:
+						if i % 2 == 0:	self.press(Direction.RIGHT, wait=0.2)
+						else:			self.press(Direction.LEFT, wait=0.2)
+				if i < (self.row - 1):
+					self.press(Direction.DOWN, wait=0.2)
 
-				if not j == self.col - 1:
-					if i % 2 == 0:	self.press(Direction.RIGHT, wait=0.2)
-					else:			self.press(Direction.LEFT, wait=0.2)
-
-			self.press(Direction.DOWN, wait=0.2)
+			# Go to next Box
+			if box < (self.boxes - 1):
+				self.press(Button.B, wait=2)
+				self.press(Button.R, wait=3)
+				self.press(Button.R, wait=0.2)
 
 		# Return from pokemon box
+		print("Released all boxes. OCR Fails: "+str(self.ocr_fails))
+		if self.shinies > 0:
+			print("SHINY POKEMON: "+str(self.shinies))
 		self.press(Button.B, wait=2)
 		self.press(Button.B, wait=2)
 		self.press(Button.B, wait=1.5)
 
 	def Release(self):
-		self.press(Button.A, wait=0.5)
-		self.press(Direction.UP, wait=0.2)
-		self.press(Direction.UP, wait=0.2)
+		self.press(Button.A, wait=0.4)
+		self.press(Direction.UP, wait=0.1)
+		self.press(Direction.UP, wait=0.1)
 		self.press(Button.A, wait=1)
-		self.press(Direction.UP, wait=0.2)
-		self.press(Button.A, wait=1.5)
-		self.press(Button.A, wait=0.3)
+		self.press(Direction.UP, wait=0.1)
+		self.press(Button.A, wait=1.4)
+		self.press(Button.A)
+
+	# marks with a red diamond, the sixth symbol
+	def MarkPerfect(self):
+		self.press(Button.A, wait=0.4)
+		self.press(Direction.DOWN, wait=0.1)
+		self.press(Direction.DOWN, wait=0.1)
+		self.press(Direction.DOWN, wait=0.1)
+		self.press(Button.A, wait=0.7)
+		self.press(Direction.LEFT)
+		self.press(Button.A, wait=0.2)
+		self.press(Button.A, duration=0.2)
+		self.press(Button.B, wait=0.2)
