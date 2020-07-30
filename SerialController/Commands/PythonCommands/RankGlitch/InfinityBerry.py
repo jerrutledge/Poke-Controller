d2c91c5a848d2469d8a57836da58f0f0ac9149be
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2020-07-30 19:37:16 -0300

    Update InfinityBerry with OCR support, bug fixes, and increased verbosity

--- SerialController/Commands/PythonCommands/RankGlitch/InfinityBerry.py
+++ SerialController/Commands/PythonCommands/RankGlitch/InfinityBerry.py
@@ -3,6 +3,8 @@
 
 from Commands.PythonCommandBase import PythonCommand, ImageProcPythonCommand
 from Commands.Keys import KeyPress, Button, Direction, Stick
+import cv2
+import time
 
 # using RankBattle glitch
 # Infinity getting berries
@@ -15,73 +17,83 @@ class InfinityBerry(ImageProcPythonCommand):
 		self.cam = cam
 
 	def do(self):
-		while True:
-
-			# If camera is not opened, then pick 1 and timeleap
-			if not self.cam.isOpened():
+		# If camera is not opened, then pick 1 and timeleap (never "Shake it more")
+		if not self.cam.isOpened():
+			while True:
 				self.press(Button.A, wait=0.5)
 				self.press(Button.B, wait=0.5)
 				self.press(Button.A, wait=0.5) # yes
-
-				for _ in range(0, 15):  # B loop
-					self.press(Button.B, wait=0.5)
+				# exit dialogue
+				self.pressRep(Button.B, 15, interval=0.5)
 
 				# Time glitch
 				self.timeLeap()
 
-			else:
-				self.press(Button.A, wait=0.5)
-				self.press(Button.B, wait=0.5)
-				self.press(Button.A, wait=0.5) # yes
-
+		else:
+			while True:
+				# talk to tree
+				self.press(Button.A, duration=0.5)
 				while True:
-					self.press(Button.A, wait=0.5) # for press 'shake more'
-					self.press(Button.A, wait=0.5) # just in case
-					self.press(Button.A, wait=0.5)
-
-					while not self.isContainTemplate('fell_message.png'):
+					text = self.getNextText()
+					if "It's a tree" in text:
+						print("1 - Shaking tree")
+						# two possible states: 
+						# 1. A down arrow that indicates the dialogue can be advanced
+						# 2. A (Yes/No) menu
+						self.press(Button.A, duration=0.3)
+						self.press(Button.A)
+						# if we press A twice quickly it covers both cases:
+						# 1. Brings up the menu and selects yes
+						# 2. Selects yes and the second input is eaten by the animation
+						self.wait(4) # wait for the shaking animation to finish
+					if "nothing" in text:
+						print("Tree is currently empty. Advancing the date...")
 						self.press(Button.B, wait=0.5)
-					print('fell message!')
-					self.press(Button.A, wait=0.5)
-
-					# Judge continuity by tree shaking motion
-					if self.isContinue():
-						print('continue')
-						self.wait(0.5)
-						continue
-					else:
-						print('not continue')
 						break
-
-				for _ in range(0, 10):  # B loop
-					self.press(Button.B, wait=0.5)
+					if "fell from the tree" in text:
+						print('2 - Something fell: '+text)
+					if "fallen to the ground" in text:
+						if self.isContinue():
+							print('3 - Continue shaking tree')
+							self.press(Button.A, wait=4)
+							continue
+						else:
+							print('4 - Collect berries and advance the date')
+							self.pressRep(Button.B, 10, interval=0.5)
+							break
 
 				# Time glitch
 				self.timeLeap()
 
 	def isContinue(self, check_interval=0.1, check_duration=2):
-		time = 0
-		zero_cnt = 0
+		check_time = 0
+		zero_count = 0
 		height_half = int(self.camera.capture_size[1] / 2)
 
 		frame1 = cv2.cvtColor(self.camera.readFrame()[0:height_half-1, :], cv2.COLOR_BGR2GRAY)
-		sleep(check_interval / 3)
+		time.sleep(check_interval / 3)
 		frame2 = cv2.cvtColor(self.camera.readFrame()[0:height_half-1, :], cv2.COLOR_BGR2GRAY)
-		sleep(check_interval / 3)
+		time.sleep(check_interval / 3)
 		frame3 = cv2.cvtColor(self.camera.readFrame()[0:height_half-1, :], cv2.COLOR_BGR2GRAY)
 
-		while time < check_duration:
+		while check_time < check_duration:
 			mask = self.getInterframeDiff(frame1, frame2, frame3, 15)
-			zero_cnt += cv2.countNonZero(mask)
+			zero_count += cv2.countNonZero(mask)
 
 			frame1 = frame2
 			frame2 = frame3
-			sleep(check_interval)
+			time.sleep(check_interval)
 			frame3 = cv2.cvtColor(self.camera.readFrame()[0:height_half-1, :], cv2.COLOR_BGR2GRAY)
 
-			time += check_interval
+			check_time += check_interval
 
-		print('diff cnt: ' + str(zero_cnt))
+		print('InfinityBerry.isContinue: zero_count=' + str(zero_count))
 
 		# zero count threshold is heuristic value... weather: sunny
-		return True if zero_cnt < 9000 else False
+		return True if zero_count < 9000 else False
+
+	# advances the dialogue by one box and returns the next text
+	def getNextText(self):
+		self.press(Button.A, duration=0.5)
+		self.wait(self.stream_delay)
+		return self.getText(top=-130, bottom=30, left=50, right=50)
